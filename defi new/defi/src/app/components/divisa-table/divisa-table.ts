6667a86d973755e9928e22322cda2990171a6cc2
Luciano jofre <165217261+skyzero18@users.noreply.github.com>
2025-06-20 19:31:13 -0300

    Setteando proyecto divisas

---
+++ defi new/defi/src/app/components/divisa-table/divisa-table.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-divisa-table',
+  imports: [],
+  templateUrl: './divisa-table.html',
+  styleUrl: './divisa-table.css'
+})
+export class DivisaTable {
+
+}
